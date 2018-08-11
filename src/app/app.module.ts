1b2672f99dee6b400c4f8921a7f6656d3b65845f
Mohamad Al Asmar <mohamad.alasmar@aqeed.com>
2018-08-11 11:34:05 +0300

    added admob demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { AdMobFree } from '@ionic-native/admob-free';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -24,6 +24,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    AdMobFree,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,47 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { AdMobFree, AdMobFreeBannerConfig, AdMobFreeInterstitialConfig } from '@ionic-native/admob-free';
+ 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
-  }
-
+ 
+    constructor(public navCtrl: NavController, public admob: AdMobFree) {
+ 
+    }
+ 
+    showBanner() {
+ 
+        let bannerConfig: AdMobFreeBannerConfig = {
+            isTesting: true, // Remove in production
+            autoShow: true
+            //id: Your Ad Unit ID goes here
+        };
+ 
+        this.admob.banner.config(bannerConfig);
+ 
+        this.admob.banner.prepare().then(() => {
+            // success
+        }).catch(e => console.log(e));
+ 
+    }
+ 
+    launchInterstitial() {
+ 
+        let interstitialConfig: AdMobFreeInterstitialConfig = {
+            isTesting: true, // Remove in production
+            autoShow: true
+            //id: Your Ad Unit ID goes here
+        };
+ 
+        this.admob.interstitial.config(interstitialConfig);
+ 
+        this.admob.interstitial.prepare().then(() => {
+            // success
+        });
+ 
+    }
+ 
 }
